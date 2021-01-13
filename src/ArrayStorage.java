c2d161f7b00abc87c554a744795d7ccadfbc94c5
Alexey Ivanov <4985900@gmail.com>
2021-01-13 09:46:05 +0300

    feat: реализация методов класса ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,55 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        int lastIndex = size();
+
+        storage[lastIndex] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        return storage[getIndex(uuid)];
     }
 
     void delete(String uuid) {
+        int lastResumeIndex = size() - 1;
+        int currentResumeIndex = getIndex(uuid);
+
+        storage[currentResumeIndex] = storage[lastResumeIndex];
+        storage[lastResumeIndex] = storage[size()];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int quantityOfResume = 0;
+
+        for (Resume resume : storage) {
+            if (resume != null) {
+                quantityOfResume += 1;
+            }
+        }
+
+        return quantityOfResume;
+    }
+
+    int getIndex(String uuid) {
+        int index = -1;
+
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                index = i;
+            }
+        }
+
+        return index;
     }
 }
