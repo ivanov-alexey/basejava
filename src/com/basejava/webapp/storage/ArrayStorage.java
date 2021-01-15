983e792f4cede6e858d327e4fa9ec1ec464367c0
Alexey Ivanov <4985900@gmail.com>
2021-01-15 07:34:04 +0300

    feat: создал пакеты, добавил модификаторы доступа

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -1,25 +1,29 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10_000];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume resume) {
+    public void save(Resume resume) {
         int lastIndex = size;
 
         storage[lastIndex] = resume;
         size ++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         if (getIndex(uuid) >= 0) {
             return storage[getIndex(uuid)];
         }
@@ -27,29 +31,30 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = getIndex(uuid);
 
         storage[index] = storage[size - 1];
+        storage[size - 1] = null;
         size --;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
-    int getIndex(String uuid) {
+    private int getIndex(String uuid) {
         int index = -1;
 
         for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 index = i;
             }
         }
