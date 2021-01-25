0c5ecd5ef02f9db79dcc7612ef4ab795d94a7230
Alexey Ivanov <4985900@gmail.com>
2021-01-25 16:14:55 +0300

    feat: знакомство с reflection api

---
+++ src/MainReflection.java
@@ -0,0 +1,22 @@
+import com.basejava.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
+        System.out.println(field.get(resume));
+
+        // вызвать r.toString через reflection
+        Method toStringMethod = Resume.class.getMethod("toString");
+        Object result = toStringMethod.invoke(resume);
+        System.out.println(result);
+    }
+}
